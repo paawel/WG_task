1018a805758dc57ac4ec8658052a6b5c02740f4a
paawel <rsk.pawel@gmail.com>
2019-06-23 23:05:05 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,141 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const CONFIG = {
+  ELEMENTS: 300
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'list';
+
+export class AppComponent implements OnInit {
+  makeChoice: any;
+  list: Array = [];
+  filteredList: Array = [];
+  selectedList: Array = [];
+  tempSelectedList: Array = [];
+  componentData: Object = {};
+
+  private _searchElement: string;
+
+  get searchElement(): string {
+    return this._searchElement;
+  }
+
+  set searchElement(value: string) {
+    this._searchElement = value;
+    this.filteredList = this.filterList(value);
+  }
+
+  constructor() {
+    this.componentData = {
+      main: {
+        button: {
+          text: "Изменить мой выбор",
+          action: this.triggerUpdateBlock.bind(this),
+          remove: "x"
+        },
+        selected: {
+          text: "На данный момент элементов выбрано",
+          makeChoice: "Сделайте свой выбор, нажмите кнопку ниже"
+        },
+        title: {
+          text: "Выбор элементов"
+        }
+      },
+      update: {
+        container: {
+          show: false
+        },
+        header: {
+          text: "Диалог выбора элементов",
+          button: {
+            text: "X",
+            action: this.triggerUpdateBlock.bind(this)
+          }
+        },
+        filter: {
+          byName: "Поиск"
+        },
+        buttons: {
+          save: {
+            text: "Сохранить",
+            action: this.saveUpdate.bind(this)
+          },
+          cancel: {
+            text: "Отмена",
+            action: this.cancelUpdate.bind(this)
+          }
+        },
+        temporary: {
+          text: "Выбранные элементы на данный момент:"
+        }
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.list = this.createData(CONFIG.ELEMENTS);
+    this.filteredList = this.list;
+  }
+
+  createData(length) {
+    let _arr: Array = [];
+
+    for (let i = 1; i <= length; i++) {
+      _arr.push({
+        name:`Элемент ${i}`,
+        id: `el${i}`,
+        selected: false
+      });
+    }
+
+    return _arr;
+  }
+
+  triggerUpdateBlock() {
+    this.componentData.update.container.show = !this.componentData.update.container.show;
+  }
+
+  saveUpdate() {
+    this.selectedList = this.tempSelectedList;
+
+    this.triggerUpdateBlock();
+  }
+
+  cancelUpdate() {
+    this.triggerUpdateBlock();
+  }
+
+  filterList(search: string) {
+    return this.list.filter(element => {
+      return element.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    })
+  }
+
+  selectElement(el) {
+    el.selected = !el.selected;
+
+    this.tempSelectedList = this.list.filter(el => {
+      return el.selected === true;
+    });
+  }
+
+  removeElementFromSelected(el) {
+    for(let i = 0; i < this.selectedList.length; i++) {
+      if (this.selectedList[i].id === el.id) {
+        this.selectedList.splice(i, 1);
+      }
+    }
+  }
+
+  removeElementFromTemporarySelected(el) {
+    for(let i = 0; i < this.tempSelectedList.length; i++) {
+      if (this.tempSelectedList[i].id === el.id) {
+        this.tempSelectedList.splice(i, 1);
+      }
+    }
+  }
 }
